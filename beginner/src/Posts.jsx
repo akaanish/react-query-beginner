364d1398fb219fcc1dbe0abea88635c62e347af9
Anish Kumar <anish.kumar067@gmail.com>
2022-12-18 11:30:25 +0530

    - added pagination support

--- beginner/src/Posts.jsx
+++ beginner/src/Posts.jsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { PostDetail } from "./PostDetail";
+import { useEffect } from "react";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(currentPage) {
     const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+        `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${currentPage}`
     );
     return response.json();
 }
 
 export function Posts() {
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const [selectedPost, setSelectedPost] = useState(null);
 
+    // prefetching
+    const queryClient = useQueryClient();
+    useEffect(() => {
+        if (currentPage < maxPostPage) {
+            const nextPage = currentPage + 1;
+            queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage))
+        }
+    }, [currentPage, queryClient]);
+
     // using react query to fetch data
-    const { data, isLoading, isError, error } = useQuery(["posts"], fetchPosts, {
-        staleTime: 5000
+    const { data, isLoading, isError, error, isPreviousData } = useQuery(["posts", currentPage], () => fetchPosts(currentPage), {
+        staleTime: 5000,
+        keepPreviousData: true
     });
-    if(isLoading) return <h3>Loading...</h3>;
+    if (isLoading) return <h3>Loading...</h3>;
     // By default - react query retries api 3 times. It is also configurable
-    if(isError) return <><h3>Oops, Something went wrong</h3><p>{error.toString()}</p></>;
+    if (isError) return <><h3>Oops, Something went wrong</h3><p>{error.toString()}</p></>;
 
     return (
         <>
@@ -36,11 +47,11 @@ export function Posts() {
                 ))}
             </ul>
             <div className="pages">
-                <button disabled onClick={() => { }}>
+                <button disabled={currentPage <= 1} onClick={() => { setCurrentPage(prev => prev - 1) }}>
                     Previous page
                 </button>
-                <span>Page {currentPage + 1}</span>
-                <button disabled onClick={() => { }}>
+                <span>Page {currentPage}</span>
+                <button disabled={currentPage >= maxPostPage} onClick={() => { if(!isPreviousData) setCurrentPage(prev => prev + 1) }}>
                     Next page
                 </button>
             </div>
